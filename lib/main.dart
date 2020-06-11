c063b761ac19a1a64d0db042b9fdf616444385f2
David-B-Comer <davec2233@gmail.com>
2020-06-11 14:19:26 -0400

    added friendly chat app widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    FriendlyChatApp(),
+  );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'FriendlyChat',
       home: Scaffold(
         appBar: AppBar(
           title: Text('FriendlyChat'),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
