d1ebfe2b9f297117bb556bbd524facba113a9c11
David-B-Comer <davec2233@gmail.com>
2020-06-14 14:05:14 -0400

    added size transition widget

--- lib/main.dart
+++ lib/main.dart
@@ -108,27 +108,31 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-    margin: EdgeInsets.symmetric(vertical: 10.0),
-    child: Row(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    Container(
-    margin: const EdgeInsets.only(right: 16.0),
-    child: CircleAvatar(child: Text(_name[0])),
-    ),
-    Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    Text(_name, style: Theme.of(context).textTheme.headline4),
-    Container(
-    margin: EdgeInsets.only(top: 5.0),
-    child: Text(text),
-    ),
-    ],
-    ),
-    ],
-    ),
+    return SizeTransition(
+      sizeFactor:
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: CircleAvatar(child: Text(_name[0])),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_name, style: Theme.of(context).textTheme.headline4),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
